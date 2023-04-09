406c944e1bc4f3a8d1a428d263d5a4e398159d7b
Ricardo Ruiz <ricardo.w.ruiz@gmail.com>
2023-04-09 06:08:19 -0700

    feat(fly.io): solve unique-ids challenge (#2)
    uses Uuid Crate

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,42 @@
 use rust_distributed_sys_challenge::*;
 
-use anyhow::{bail, Context, Ok};
+use anyhow::{Context, Ok};
 use serde::{Deserialize, Serialize};
 use std::io::{StdoutLock, Write};
+use uuid::{self, Uuid};
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(tag = "type")] // IMPORTANT: returns {type:"echo", echo:"..."}
 #[serde(rename_all = "snake_case")]
 enum PayLoad {
+    //NOTE: find a way to remoke OKs from this enum
     Echo {
         echo: String,
     },
     EchoOk {
         echo: String,
     },
-    // IMPORTANT: these cases need to be handled to run test suite
-    // NOTE: these have to be handled because we aren't using the go library from tutorial
-    Init {
-        node_id: String,
-        node_ids: Vec<String>,
+    Generate,
+    GenerateOk {
+        #[serde(rename = "id")]
+        guid: Uuid,
     },
-    InitOk,
 }
 
 struct DistributedNode {
-    id: usize,
+    node: Option<String>,
+    id: u8,
 }
 
 // NOTE: state machine
-impl Node<PayLoad> for DistributedNode {
+impl Node<(), PayLoad> for DistributedNode {
+    fn from_init(_state: (), init: InitNodes) -> anyhow::Result<Self> {
+        return Ok(DistributedNode {
+            node: init.node_id,
+            id: 1,
+        });
+    }
+
     fn step(&mut self, input: Message<PayLoad>, output: &mut StdoutLock) -> anyhow::Result<()> {
         match input.body.payload {
             | PayLoad::Echo { echo } => {
@@ -47,30 +55,28 @@ impl Node<PayLoad> for DistributedNode {
                 self.id += 1;
             },
             | PayLoad::EchoOk { .. } => {},
-
-            | PayLoad::Init { .. } => {
+            | PayLoad::Generate => {
+                let guid = Uuid::now_v1(&[self.id; 6]);
                 let reply = Message {
                     src: input.dest,
                     dest: input.src,
                     body: Body {
                         id: Some(self.id),
                         in_reply_to: input.body.id,
-                        payload: PayLoad::InitOk,
+                        payload: PayLoad::GenerateOk { guid },
                     },
                 };
                 serde_json::to_writer(&mut *output, &reply)
-                    .context("Serialize response to init")?;
-                output.write_all(b"\n").context("Write trailing newline")?;
+                    .context("serialize response to generate")?;
+                output.write_all(b"\n").context("write trailing newline")?;
                 self.id += 1;
             },
-
-            // NOTE: this should only be used by Maelstrom client
-            | PayLoad::InitOk { .. } => bail!("Recieved init_ok message. This shouldn't happen"),
+            | PayLoad::GenerateOk { .. } => {},
         }
         return Ok(());
     }
 }
 
 fn main() -> anyhow::Result<()> {
-    event_loop(DistributedNode { id: 0 })
+    return event_loop::<DistributedNode, _, _>(());
 }
