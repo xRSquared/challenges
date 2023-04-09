00aa84537b750d899207f9b910b7b6f3e9dfec47
Ricardo Ruiz <ricardo.w.ruiz@gmail.com>
2023-04-08 22:59:02 -0700

    tests: allow arguments to test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env fish
+set test $argv;
 
-~/maelstrom/maelstrom test -w echo --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
+if test "$test" = "echo"
+    ~/maelstrom/maelstrom test -w echo --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
+end
+if test "$test" = "unique-ids"
+    ~/maelstrom/maelstrom test -w unique-ids --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
+end
