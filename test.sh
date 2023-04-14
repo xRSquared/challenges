6e827f56130b63823dcc3a5f4bdc3f1e384b9345
Ricardo Ruiz <ricardo.w.ruiz@gmail.com>
2023-04-13 22:30:36 -0700

    test(fly.io): add multi-broadcast test and switch statement

--- test.sh
+++ test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env fish
 set test $argv;
 
-if test "$test" = "echo"
-    ~/maelstrom/maelstrom test -w echo --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
-end
-if test "$test" = "unique-ids"
-    ~/maelstrom/maelstrom test -w unique-ids --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
-end
-if test "$test" = "broadcast"
-    ~/maelstrom/maelstrom test -w broadcast --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 20 --rate 10
+switch "$test"
+    case "echo"
+        ~/maelstrom/maelstrom test -w echo --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
+    case "unique-ids"
+        ~/maelstrom/maelstrom test -w unique-ids --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 10
+    case "single-broadcast"
+        ~/maelstrom/maelstrom test -w broadcast --bin ./target/debug/rust-distributed-sys-challenge --node-count 1 --time-limit 20 --rate 10
+    case "multi-broadcast"
+        ~/maelstrom/maelstrom test -w broadcast --bin ./target/debug/rust-distributed-sys-challenge --node-count 5 --time-limit 20 --rate 10
+    case '*'
+        echo unknown test $test
 end
